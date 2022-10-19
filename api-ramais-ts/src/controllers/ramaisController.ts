8249b146c451b1cb5718cbff1ec5400314e1a626
anamrcnds <ana.marcondes01@fatec.sp.gov.br>
2022-10-19 08:10:54 -0300

    Estrutura modificada

---
+++ api-ramais-ts/src/controllers/ramaisController.ts
@@ -0,0 +1,127 @@
+import { Request, Response } from 'express'
+import Ramal, { IRamal }  from '../models/Ramais'
+
+//Lista todos os registros
+
+const getListarRegistros = async (request: Request, response: Response): Promise<void> => {
+
+    try{
+        const ramais: Array<IRamal> = await Ramal.find()
+
+        return response.send_ok('Registros encontrados com sucesso!', { ramais })
+        
+    }catch {
+        return response.send_internalServerError('Ocorreu um erro')
+    }
+}
+
+// Lista o nome e o departamento de um funcionário de acordo com o número do ramal fornecido
+const getNomeDep = async (request: Request, response: Response): Promise<void> => {
+    
+    const ramal: number = Number(request.params.ramal)
+    
+        try {
+            const funcionario: IRamal | null = await Ramal.findOne( { ramal: ramal }, { nome: 1, departamento: 1})
+            
+            if(!funcionario) return response.send_unprocessableEntity('Registro inexistente!')
+        
+            return response.send_ok('Registro encontrado com sucesso!', { funcionario })
+
+        } catch {
+            return response.send_internalServerError('Ocorreu um erro!')
+        }
+    }
+
+// Busca um funcionário a partir de uma parte de seu nome
+
+const getTrechoNome = async (request: Request, response: Response): Promise<void> => {
+        
+        const string: string = String(request.params.string)
+        
+        try {
+            // Linha 55 não aceitou IRamal | null, apenas tipo object - a investigar
+            const funcionario: Array<IRamal> = await Ramal.find( { nome: {"$regex": `${string}` , "$options": "i"}}, {} )
+            
+            if(!funcionario) return response.send_unprocessableEntity('Registro inexistente!')
+            
+            return response.send_ok('Registro encontrado com sucesso!', {funcionario})
+            
+        } catch {
+            return response.send_internalServerError('Ocorreu um erro!')
+        }
+    }
+
+// Cria um novo registro
+const createRegistro = async (request: Request, response: Response): Promise<void> => {
+        
+        
+    const { nome, ramal, departamento }: { nome: string, ramal: number, departamento: string } = request.body
+    const dataCriacao: string = Date();
+    const dataUltimaAtualizacao: string = Date()
+    
+    if(!nome) return response.send_unprocessableEntity('Insira nome, ramal e departamento!')
+    
+    const novoRegistro: IRamal = {nome, ramal, departamento, dataUltimaAtualizacao, dataCriacao}
+    
+    try {
+        await Ramal.create(novoRegistro)
+        
+        return response.send_created('Registro adicionado com sucesso!', {novoRegistro})
+        
+    } catch {
+        return response.send_internalServerError('Ocorreu um erro!')
+    }
+}
+   
+// Atualiza um novo registro já existente de acordo com o número do ramal fornecido
+const updateRegistro = async (request: Request, response: Response): Promise<void> => {
+    const ramalUrl: number = Number(request.params.ramal)
+    
+    const { nome, ramal, departamento, dataCriacao }
+        : {nome: string, ramal: number, departamento: string, dataCriacao: string} = request.body
+        
+        const dataUltimaAtualizacao: string = Date()
+        
+    //Verifica se o ramal existe na database
+    const ramalValido = await Ramal.exists({ ramal: ramalUrl})
+    if(!ramalValido) return response.send_badRequest('Ramal inexistente!')
+    
+    try {
+        const atualizacoes: IRamal= { nome, ramal, departamento, dataUltimaAtualizacao , dataCriacao }
+        
+        await Ramal.updateOne({ ramal: ramalUrl }, atualizacoes)
+        
+        return response.send_ok('Registro atualizado com sucesso!', {atualizacoes})
+        
+    } catch {
+        return response.send_internalServerError('Ocorreu um erro!')
+    }
+}
+
+// Exclui um registro de acordo com o número do ramal fornecido
+const deleteRegistro = async (request: Request, response: Response): Promise<void> => {
+    const ramalUrl: number = Number(request.params.ramal)
+    
+    //Verifica se o ramal existe na database
+    const registroValido = await Ramal.exists({ ramal: ramalUrl})
+    if(!registroValido) return response.send_badRequest('Ramal não encontrado!')
+    
+    try {
+
+        await Ramal.deleteOne({ ramal: ramalUrl })
+        
+        return response.send_ok('Registro removido!')
+    } catch {
+
+        return response.send_internalServerError('Ocorreu um erro!')
+    }
+}
+
+export {
+	getListarRegistros,
+	getNomeDep,
+	getTrechoNome, 
+	createRegistro,
+	updateRegistro,
+	deleteRegistro
+}
---
+++ api-ramais-ts/src/tests/ramaisTest..ts
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import supertest from 'supertest'
+import dotenv from 'dotenv'
+import app from '../app'
+
+dotenv.config()
+
